6797338bfc6842590e443c00543103d01af02376
taxio <taxio.2029@gmail.com>
2023-05-10 15:43:34 +0900

    update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,21 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  gasReporter: {
+    enabled: !!process.env.REPORT_GAS,
+    currency: "USD",
+    token: process.env.GAS_REPORTER_TOKEN,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  }
 };
 
 export default config;
